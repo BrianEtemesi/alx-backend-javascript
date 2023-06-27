376e96f6c066fcc7bb63d04f9e2865581df765bb
BrianEtemesi <etemecci@gmail.com>
2023-06-27 13:18:58 +0000

    add lint exceptions

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,5 +1,6 @@
 #!/usr/bin/env node
-
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-var */
 export default function taskBlock(trueOrFalse) {
   const task = false;
   const task2 = true;
