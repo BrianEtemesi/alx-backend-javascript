8011e1b0af11890f79a6e3437e3259977ad4cc01
BrianEtemesi <etemecci@gmail.com>
2023-06-28 17:37:32 +0000

    create a report object

---
+++ 0x00-ES6_basic/test.js
@@ -0,0 +1,8 @@
+import createEmployeesObject from './11-createEmployeesObject.js';
+
+const employees = {
+    ...createEmployeesObject('engineering', ['Bob', 'Jane']),
+    ...createEmployeesObject('marketing', ['Sylvie'])
+};
+
+console.log(employees);
