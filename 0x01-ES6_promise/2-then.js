d398f5f225f6de2a11663bcca5322283404e259c
BrianEtemesi <etemecci@gmail.com>
2023-06-29 21:49:01 +0000

    use finally()

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,9 +1,10 @@
+/* eslint-disable no-unused-vars */
 export default function handleResponseFromAPI(promise) {
-  promise.then((response) => {
+  const successObj = {
+    status: 200,
+    body: 'success',
+  };
+  promise.then((response) => successObj).catch((error) => error).finally(() => {
     console.log('Got a response from the API');
-    return response;
-  }).catch((error) => {
-    console.log('Got a response from the API');
-    return error;
   });
 }
