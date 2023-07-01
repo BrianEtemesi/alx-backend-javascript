65cb4f634156a7d42999c7757bcb2faf7ae0ad0f
BrianEtemesi <etemecci@gmail.com>
2023-07-01 18:48:40 +0300

    call functions properly

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,7 +1,7 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
-  return Promise.all([uploadPhoto, createUser]).then((obj) => {
+  return Promise.all([uploadPhoto(), createUser()]).then((obj) => {
     console.log(`${obj[0].body} ${obj[1].firstName} ${obj[1].lastName}`);
   }).catch(() => {
     console.log('Signup system offline');
