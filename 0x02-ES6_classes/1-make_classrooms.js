918b2a2e732dadc436f876c7c846a0248f68b00e
BrianEtemesi <etemecci@gmail.com>
2023-07-02 18:50:44 +0000

    instantiate some classrooms

---
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -0,0 +1,10 @@
+import ClassRoom from './0-classroom';
+
+export default function initializeRooms() {
+  const classRooms = [];
+  const sizes = [19, 20, 34];
+  for (const size of sizes) {
+    classRooms.push(size);
+  }
+  return classRooms;
+}
