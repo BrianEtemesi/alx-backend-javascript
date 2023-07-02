9536c157ec59e548aae7da83b8e99e92e949f4e2
BrianEtemesi <etemecci@gmail.com>
2023-07-02 17:29:42 +0000

    handle eslint errors

--- 0x01-ES6_promise/100-await.js
+++ 0x01-ES6_promise/100-await.js
@@ -3,12 +3,12 @@ import { uploadPhoto, createUser } from './utils';
 export default async function asyncUploadUser() {
   const userObj = {};
   try {
-    const promise = await  Promise.all([uploadPhoto(), createUser()]);
+    const promise = await Promise.all([uploadPhoto(), createUser()]);
     userObj.photo = promise[0];
-	userObj.user = promise[1];
+    userObj.user = promise[1];
   } catch (error) {
     userObj.photo = null;
-	userObj.user = null;
+    userObj.user = null;
   }
   return userObj;
 }
