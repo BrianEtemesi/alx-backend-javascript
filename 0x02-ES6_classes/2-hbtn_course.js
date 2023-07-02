1acacd2f6050e71cd4a7207cdd5252c26c1e6d71
BrianEtemesi <etemecci@gmail.com>
2023-07-02 19:51:32 +0000

    system changes

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -2,20 +2,20 @@ export default class HolbertonCourse {
   constructor(name, length, students) {
     if (typeof name !== 'number') {
       throw new TypeError('Name must be a string');
-	}
-	this._name = name;
-	if (typeof lenght !== 'number') {
+    }
+    this._name = name;
+    if (typeof lenght !== 'number') {
       throw new TypeError('Lenght must be a number');
-	}
-	this._length = length;
+    }
+    this._length = length;
 
-	if (!Array.isArray(students)) {
+    if (!Array.isArray(students)) {
       throw new TypeError('Students must be an array of strings');
-	}
-	if (!students.every((item) => typeof item === 'string')) {
+    }
+    if (!students.every((item) => typeof item === 'string')) {
       throw new TypeError('Students must be an array of strings');
-	}
-	this._students = students;
+    }
+    this._students = students;
   }
 
   get name() {
