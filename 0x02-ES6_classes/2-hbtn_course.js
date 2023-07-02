31a0a1de05ad4725bb5956193bfeece63aa37b45
BrianEtemesi <etemecci@gmail.com>
2023-07-02 19:56:47 +0000

    correct typeof name check

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,6 +1,6 @@
 export default class HolbertonCourse {
   constructor(name, length, students) {
-    if (typeof name !== 'number') {
+    if (typeof name !== 'string') {
       throw new TypeError('Name must be a string');
     }
     this._name = name;
