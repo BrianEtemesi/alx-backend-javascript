1dd92ff5018fb96a2c034ca37a385a346ebd4777
BrianEtemesi <etemecci@gmail.com>
2023-07-03 20:27:01 +0000

    fix eslint errors

--- 0x02-ES6_classes/7-airport.js
+++ 0x02-ES6_classes/7-airport.js
@@ -2,12 +2,12 @@ export default class Airport {
   constructor(name, code) {
     if (typeof name !== 'string') {
       throw new TypeError('Name must be a string');
-	}
-	if (typeof code !== 'string') {
+    }
+    if (typeof code !== 'string') {
       throw new TypeError('Code must be a string');
-	}
-	this._name = name;
-	this._code = code;
+    }
+    this._name = name;
+    this._code = code;
   }
 
   // default string description method
