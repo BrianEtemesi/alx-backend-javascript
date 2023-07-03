c0f08f1f8714ddfbcaa4a0d9e3a0a037ef2cb1ed
BrianEtemesi <etemecci@gmail.com>
2023-07-03 19:46:16 +0000

    remove quotes from Building

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,12 +1,12 @@
 export default class Building {
   constructor(sqft) {
-    if (this.constructor !== 'Building' && !(this.evacuationWarningMessage)) {
+    if (this.constructor !== Building && !(this.evacuationWarningMessage)) {
       throw new Error('Class extending Building must override evacuationWarningMessage');
-	}
-	if (typeof sqft !== 'number') {
+    }
+    if (typeof sqft !== 'number') {
       throw new TypeError('Sqft must be a number');
-	}
-	this._sqft = sqft;
+    }
+    this._sqft = sqft;
   }
 
   // getter method for sqft attribute
