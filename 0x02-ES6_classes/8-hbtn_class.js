c8bba55c5a287071661e66d12d1b47781bdc2d01
BrianEtemesi <etemecci@gmail.com>
2023-07-04 08:58:05 +0000

    fix eslint errors

--- 0x02-ES6_classes/8-hbtn_class.js
+++ 0x02-ES6_classes/8-hbtn_class.js
@@ -2,12 +2,12 @@ export default class HolbertonClass {
   constructor(size, location) {
     if (typeof size !== 'number') {
       throw new TypeError('Size must be a number');
-	}
-	if (typeof location !== 'string') {
+    }
+    if (typeof location !== 'string') {
       throw new TypeError('Location must be a string');
-	}
-	this._size = size;
-	this._location = location;
+    }
+    this._size = size;
+    this._location = location;
   }
 
   // method to cast instance to a number
