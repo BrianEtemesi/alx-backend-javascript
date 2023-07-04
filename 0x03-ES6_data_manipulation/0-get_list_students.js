159aa653534ca71466fb28d6b2a2b4bcf3ff72df
BrianEtemesi <etemecci@gmail.com>
2023-07-04 21:42:06 +0000

    set up environment

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,21 +1,21 @@
 export default function getListStudents() {
   const s1 = {
     id: 1,
-	firstName: 'Guilaume',
-	location: 'San Francisco'
-  }
+    firstName: 'Guilaume',
+    location: 'San Francisco',
+  };
 
   const s2 = {
     id: 2,
-	firstName: 'James',
-	location: 'Columbia'
-  }
+    firstName: 'James',
+    location: 'Columbia',
+  };
 
-  const 23 = {
+  const s3 = {
     id: 5,
-	firstName: 'Serena',
-	location: 'San Francisco'
-  }
+    firstName: 'Serena',
+    location: 'San Francisco',
+  };
 
   return [s1, s2, s3];
 }
