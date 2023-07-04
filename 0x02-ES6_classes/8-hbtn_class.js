3988d9eaa6fcdef964e8df8a1a0e02fb3e9c7c0e
BrianEtemesi <etemecci@gmail.com>
2023-07-04 08:46:22 +0000

    HolbertonClass call to primitive values

---
+++ 0x02-ES6_classes/8-hbtn_class.js
@@ -0,0 +1,22 @@
+export default class HolbertonClass {
+  constructor(size, location) {
+    if (typeof size !== 'number') {
+      throw new TypeError('Size must be a number');
+	}
+	if (typeof location !== 'string') {
+      throw new TypeError('Location must be a string');
+	}
+	this._size = size;
+	this._location = location;
+  }
+
+  // method to cast instance to a number
+  valueOf() {
+    return this._size;
+  }
+
+  // method to vast instance to a string
+  toString() {
+    return this._location;
+  }
+}
