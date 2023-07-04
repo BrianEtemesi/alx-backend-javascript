b68bb1b8f56ff7977a93a57c0f6b29e99fb290fc
BrianEtemesi <etemecci@gmail.com>
2023-07-04 20:36:51 +0000

    remove datatype checks and symbol object instatiation

--- 0x02-ES6_classes/10-car.js
+++ 0x02-ES6_classes/10-car.js
@@ -1,23 +1,12 @@
-const cloneCar = Symbol();
-
 export default class Car {
   constructor(brand, motor, color) {
-    if (typeof brand !== 'string') {
-      throw new TypeError('Brand must be a string');
-	}
-	if (typeof motor !== 'string') {
-      throw new TypeError('Motor must be a string');
-	}
-	if (typeof color !== 'string') {
-      throw new TypeError('Color must be a string');
-	}
-	this._brand = brand;
-	this._motor = motor;
-	this._color = color;
+    this._brand = brand;
+    this._motor = motor;
+    this._color = color;
   }
 
   // method to return new object of this class
-  [cloneCar]() {
-    return new Car(this._brand, this._motor, this._color);
+  cloneCar() {
+    return new this.constructor();
   }
 }
