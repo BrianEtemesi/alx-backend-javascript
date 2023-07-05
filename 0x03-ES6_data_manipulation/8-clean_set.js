14eca1f044c3ddbb8d3a44c5d85785ba90b82f97
BrianEtemesi <etemecci@gmail.com>
2023-07-05 13:20:36 +0000

    check is startstring is empty

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,5 +1,8 @@
 export default function cleanSet(set, startString) {
   // convert set to array
+  if (startString === '') {
+    return '';
+  }
   const filteredValues = Array.from(set)
     .filter((value) => value.startsWith(startString)) // keep only values starting with startString
     .map((value) => value.slice(startString.length)); // remove startstring from each value
