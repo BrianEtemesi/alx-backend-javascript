275a940f70d4046c57757b50a927d2213d0c58f7
BrianEtemesi <etemecci@gmail.com>
2023-07-05 12:45:51 +0000

    add parenteses around argument to arraw function

--- 0x03-ES6_data_manipulation/7-has_array_values.js
+++ 0x03-ES6_data_manipulation/7-has_array_values.js
@@ -1,4 +1,4 @@
 export default function hasValuesFromArray(set, array) {
   // iterate over array and check if value is in set
-  return array.every(value => set.has(value));
+  return array.every((value) => set.has(value));
 }
