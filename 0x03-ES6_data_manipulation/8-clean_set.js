eb544e1a52b779d860cd086cef147eae258beee8
BrianEtemesi <etemecci@gmail.com>
2023-07-05 13:15:49 +0000

    fix eslint errors

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -2,7 +2,7 @@ export default function cleanSet(set, startString) {
   // convert set to array
   const filteredValues = Array.from(set)
     .filter((value) => value.startsWith(startString)) // keep only values starting with startString
-	.map((value) => value.slice(startString.length)); // remove startstring from each value
+    .map((value) => value.slice(startString.length)); // remove startstring from each value
 
   return filteredValues.join('-');
 }
