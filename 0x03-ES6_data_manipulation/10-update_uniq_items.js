c61078d074e0dca49b12f83393ca2bc4d6ab3da7
BrianEtemesi <etemecci@gmail.com>
2023-07-05 19:49:10 +0000

    more map data structure

---
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -0,0 +1,13 @@
+export default function updateUniqueItems(myMap) {
+  try {
+    myMap.forEach((value, key, map) => {
+      if (value === 1) {
+        map.set(key, 100);
+	  }
+	})
+  } catch (e) {
+    throw new Error('Cannot process');
+  }
+
+  return myMap;
+}
