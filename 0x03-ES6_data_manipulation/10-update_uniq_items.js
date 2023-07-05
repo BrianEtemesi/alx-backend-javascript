e1dd192a940a953b1e972066a520851e20105ce4
BrianEtemesi <etemecci@gmail.com>
2023-07-05 19:53:42 +0000

    fix eslint errors

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -3,8 +3,8 @@ export default function updateUniqueItems(myMap) {
     myMap.forEach((value, key, map) => {
       if (value === 1) {
         map.set(key, 100);
-	  }
-	})
+      }
+    });
   } catch (e) {
     throw new Error('Cannot process');
   }
