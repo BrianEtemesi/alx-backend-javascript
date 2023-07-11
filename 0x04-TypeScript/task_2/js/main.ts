24b11db6e0aa5ededeb71d2447134bbf21bfeadd
BrianEtemesi <etemecci@gmail.com>
2023-07-11 09:30:04 +0000

    creating functions specific to employees

--- 0x04-TypeScript/task_2/js/main.ts
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -45,6 +45,16 @@ function createEmployee(salary: number | string): Director | Teacher {
     return new Director();
   }
 }
-console.log(createEmployee(200));
-console.log(createEmployee(1000));
-console.log(createEmployee('$500'));
+
+function isDirector(employee: Director | Teacher) {
+  return employee instanceof Director;
+}
+
+function executeWork(employee: Director | Teacher): string {
+  if (isDirector(employee)) {
+    return (employee as Director).workDirectorTasks();
+  } else {
+    return (employee as Teacher).workTeacherTasks();
+  }
+}
+
