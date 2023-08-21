4bd3b9d134c9b5227df244ebbb70a4d58dc6097b
BrianEtemesi <etemecci@gmail.com>
2023-08-21 19:54:13 +0000

    using process stdin

---
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -0,0 +1,22 @@
+#!/usr/bin/env node
+
+// Print a welcome message
+console.log('Welcome to Holberton School, what is your name?');
+
+// Listen for readable events on the standard input
+process.stdin.on('readable', () => {
+  // Read the input chunk
+  const chunk = process.stdin.read();
+
+  // Check if there's data in the chunk
+  if (chunk !== null) {
+    // Print the user's name based on the input chunk
+    console.log(`Your name is: ${chunk}`);
+  }
+});
+
+// Listen for the end event on the standard input
+process.stdin.on('end', () => {
+  // Print a closing message when the input ends
+  console.log('This important software is now closing');
+});
