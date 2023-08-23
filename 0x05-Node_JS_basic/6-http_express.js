45efe21eb55c5071794ed4b0a72458b5c3b2cd8d
BrianEtemesi <etemecci@gmail.com>
2023-08-23 11:55:17 +0000

    simple http server using express

---
+++ 0x05-Node_JS_basic/6-http_express.js
@@ -0,0 +1,14 @@
+#!/usr/bin/env node
+
+const express = require('express');
+
+const app = express();
+const port = 1245;
+
+app.get('/', (req, res) => {
+  res.send('Hello Holberton School!');
+});
+
+app.listen(port, () => {
+  console.log(`express server listening on port ${port}`);
+});
