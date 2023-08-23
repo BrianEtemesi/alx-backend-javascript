7504e9d36b65179ebb9f29d079b94cde9f945443
BrianEtemesi <etemecci@gmail.com>
2023-08-23 16:46:10 +0000

    function to read database

---
+++ 0x05-Node_JS_basic/full_server/utils.js
@@ -0,0 +1,40 @@
+#!/usr/bin/env node
+
+const fs = require('fs');
+
+const readDatabase = (filePath) => new Promise((resolve, reject) => {
+  fs.readFile(filePath, 'utf8', (err, data) => {
+    if (err) {
+      reject(new Error('Cannot load the database'));
+    }
+    const fields = {};
+    if (data) {
+      const lines = data.trim().split('\n'); // split the file into an array of lines
+      const studentData = lines.slice(1); // remove the first line (header)
+
+      // get number of students in each field
+      for (const student of studentData) {
+        const studentInfo = student.split(',');
+        const firstName = studentInfo[0];
+        const field = studentInfo[studentInfo.length - 1];
+        if (fields[field]) {
+          fields[field].push(firstName);
+        } else {
+          fields[field] = [];
+          fields[field].push(firstName);
+        }
+      }
+    }
+    resolve(fields);
+  });
+});
+
+readDatabase('database.csv')
+  .then(data => {
+    console.log(data);
+  })
+  .catch(err => {
+    console.log(err.toString);
+  })
+
+module.exports = readDatabase;
