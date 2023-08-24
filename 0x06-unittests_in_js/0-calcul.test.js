b3cf76ed4cd1c203866a6e2fa26bd7754f6c922b
BrianEtemesi <etemecci@gmail.com>
2023-08-24 12:43:01 +0000

    multiple tests

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -4,9 +4,24 @@ const calculateNumber = require('./0-calcul');
 const assert = require('assert');
 
 describe('calculateNumber', function() {
-  it('rounds two numbers and returns their sum', function() {
-    const result = calculateNumber(1.2, 3.7);
-    assert.strictEqual(result, 5);
+  it('rounds & sum; both fractional numbers', function() {
+    assert.strictEqual(calculateNumber(1.2, 3.7), 5);
+  });
+
+  it('rounds & sum; both whole numbers', function() {
+    assert.strictEqual(calculateNumber(1, 3), 4);
+  });
+
+  it('rounds & sum; both whole floating numbers', function() {
+    assert.strictEqual(calculateNumber(1.0, 3.0), 4);
+  });
+
+  it('rounds & sum; one fractional number', function() {
+    assert.strictEqual(calculateNumber(1.2, 3.0), 4);
+  });
+
+  it('rounds & sum; second fractional numbers', function() {
+    assert.strictEqual(calculateNumber(1.0, 3.7), 5);
   });
 });
 
